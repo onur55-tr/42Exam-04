c6207629f352e6c14355b0698216e4a38bb83421
Onur Dursun <58361733+onur55-tr@users.noreply.github.com>
2023-02-16 23:24:47 +0300

    Update microshell.c

--- microshell.c
+++ microshell.c
@@ -2,40 +2,41 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int tmp;
+int	tmp;
 
-int print(char *str)
+int	print(char *s)
 {
-	while (*str)
-		write(2, str++, 1);
+	while (*s)
+		write(2, s++, 1);
 	return 1;
 }
 
-int echo_error(char **str, int index)
+int echo_errno(char **str, int index)
 {
 	if (index != 2)
 		return (print("error: cd: bad arguments\n"));
 	if (chdir(str[1]))
-		return (print("error: cd: cannot chage directory to ") & print(str[1]) & print("\n"));
-	return 0;
+		return (print("error: cd: cannot change directory ") & print(str[1]) & print("\n"));
+	return (0);
 }
 
 int execute(char **str, char **env, int index)
 {
-	int tmp2[2], res;
-	int pip = (str[index] && !strcmp(str[index], "|"));
-	int pid;
+	int	tmp2[2], res, pip, pid;
 
-	if (pip && (pipe(tmp2)))
+	pip = (str[index] && !strcmp(str[index], "|"));
+	if (pip && pipe2(tmp2))
 		return (print("error: fatal\n"));
-	if (!(pid = fork())) {
+	if (!(pid = fork()))
+	{
 		str[index] = 0;
-		if ((dup2(tmp, 0) == -1) | (close(tmp) == -1) | ((pip && (dup2(tmp2[1], 1) == -1) | (close(tmp2[0]) == -1) | (close(tmp2[1]) == -1))))
+		if ((dup2(tmp, 0) == -1) | (close(tmp) == -1 )| (pip && (dup2(tmp2[1], 1) == -1) | (close(tmp2[0]) == -1) | (close(tmp2[1]) == -1)))
 			return (print("error: fatal\n"));
 		execve(*str, str, env);
 		return (print("error: cannot execute ") & print(*str) & print("\n"));
 	}
-	if (((pip && (dup2(tmp2[0], tmp) == -1) | (close(tmp2[0]) == -1) | (close(tmp2[1]) == -1))) | (!pip && dup2(0, tmp) == -1) | (waitpid(pid, &res, 0) == -1))
+	if ((pip && (dup2(tmp2[0], tmp) == -1) | (close(tmp2[0]) == -1)
+	| (close(tmp2[1]) == -1)) | (!pip && dup2(0, tmp) == -1) | (waitpid(pid, &res, 0) == -1))
 		return (print("error: fatal\n"));
 	return (WIFEXITED(res) && WEXITSTATUS(res));
 }
@@ -58,6 +59,5 @@ int main(int ac, char **av, char **env)
 		else if (index)
 			res = execute(av, env, index);
 	}
-
 	return ((dup2(0, tmp) == -1 && print("error: fatal\n")) | res);
 }
